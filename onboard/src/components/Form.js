453bfe8ffbe4956009d1bf934e29a0d194054ce2
matieuruth <matieu.ruth@gmail.com>
2020-09-10 18:27:23 -0400

    Form.js comp created

--- onboard/src/components/Form.js
+++ onboard/src/components/Form.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+    const [info, setInfo] = useState('');
     return (
-        <div>
+        <form>
+            <label htmlFor="name"></label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={info}
+            />
 
-        </div>
+            <label htmlFor="email"></label>
+            <input
+                type="text"
+                id="email"
+                name="email"
+                onChange={info}
+            />
+
+            <label htmlFor="password"></label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                onChange={info}
+            />
+
+            <input
+                type="checkbox"
+                id="checkbox"
+                name="checkbox"
+                value="terms of service"
+            />
+            <label htmlFor="checkbox">Terms of service</label>
+        
+            <button>Submit</button>
+        </form>
     )
 }
 
